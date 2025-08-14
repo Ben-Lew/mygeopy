d98549183e40fb73822cb3ebae1b6a2570b9cdac
Ben Lew <weipeng1992@hotmail.com>
2025-08-14 14:03:20 -0800

    add test.yml

--- mygeopy/triangle.py
+++ mygeopy/triangle.py
@@ -1,4 +1,12 @@
 import numpy as np
 
-def hypot(a,b):
+def hypot(a: float | np.ndarray,b: float | np.ndarray)-> float | np.ndarray:
+    '''
+    to calculate the hypotenus of a triangle
+    a: float or array
+    b: float or array
+    
+    RETURN
+    float or array 
+    '''
     return (a **2 + b**2)**0.5
